9a1e74dd1cce4898206fe6f40a7e30aeaad52f8c
MariaSarbaj <63845854+MariaSarbaj@users.noreply.github.com>
2020-09-27 20:14:33 +0300

    Исправления по заданию

--- js/setup.js
+++ js/setup.js
@@ -1,61 +1,67 @@
 'use strict';
 const WIZARD_NAMES = [`Иван`, `Хуан Себастьян`, `Мария`, `Кристоф`, `Виктор`, `Юлия`, `Люпита`, `Вашингтон`];
 const WIZARD_SURNAMES = [`да Марья`, `Верон`, `Мирабелла`, `Вальц`, `Онопко`, `Топольницкая`, `Нионго`, `Ирвинг`];
-const WIZARD_COAT = [`rgb(101, 137, 164)`, `rgb(241, 43, 107)`, `rgb(146, 100, 161)`, `rgb(56, 159, 117)`, `rgb(215, 210, 55)`, `rgb(0, 0, 0)`];
+const WIZARD_COATS = [`rgb(101, 137, 164)`, `rgb(241, 43, 107)`, `rgb(146, 100, 161)`, `rgb(56, 159, 117)`, `rgb(215, 210, 55)`, `rgb(0, 0, 0)`];
 const WIZARD_EYES = [`black`, `red`, `blue`, `yellow`, `green`];
 
 const userDialog = document.querySelector(`.setup`);
 userDialog.classList.remove(`hidden`);
 
-const similarListElement = document.querySelector(`.setup-similar-list`);
+const similarList = document.querySelector(`.setup-similar-list`);
 const similarWizardTemplate = document.querySelector(`#similar-wizard-template`).content.querySelector(`.setup-similar-item`);
 
-let randomWizardList = [];
+const randomWizardsList = [];
 
 let getRandomWizard = function () {
-  let getRandomWizardName = function (names, surnames) {
-    return (names[Math.floor(Math.random() * names.length)] + ` ` + surnames[Math.floor(Math.random() * surnames.length)]);
+  const getRandomWizardName = function (names, surnames) {
+    return `${names[Math.floor(Math.random() * names.length)]} ${surnames[Math.floor(Math.random() * surnames.length)]}`;
   };
 
-  let getRandomWizardCoat = function (colors) {
+  const getRandomWizardCoat = function (colors) {
     return colors[Math.floor(Math.random() * colors.length)];
   };
 
-  let getRandomWizardEyes = function (eyes) {
+  const getRandomWizardEyes = function (eyes) {
     return eyes[Math.floor(Math.random() * eyes.length)];
   };
 
-  for (let i = 0; i < 4; i++) {
+  const randomWizardCount = 4;
+
+  for (let i = 0; i < randomWizardCount; i++) {
     let randomWizard = {
       name: getRandomWizardName(WIZARD_NAMES, WIZARD_SURNAMES),
-      coatColor: getRandomWizardCoat(WIZARD_COAT),
+      coatColor: getRandomWizardCoat(WIZARD_COATS),
       eyeColor: getRandomWizardEyes(WIZARD_EYES)
     };
 
-    randomWizardList.push(randomWizard);
+    randomWizardsList.push(randomWizard);
   }
 
-  return randomWizardList;
+  return randomWizardsList;
 };
 
 getRandomWizard();
 
-let renderWizard = function (wizard) {
-  let wizardElement = similarWizardTemplate.cloneNode(true);
+const renderWizard = function (wizard) {
+  let wizardContent = similarWizardTemplate.cloneNode(true);
 
-  wizardElement.querySelector(`.setup-similar-label`).textContent = wizard.name;
-  wizardElement.querySelector(`.wizard-coat`).style.fill = wizard.coatColor;
-  wizardElement.querySelector(`.wizard-eyes`).style.fill = wizard.eyeColor;
+  wizardContent.querySelector(`.setup-similar-label`).textContent = wizard.name;
+  wizardContent.querySelector(`.wizard-coat`).style.fill = wizard.coatColor;
+  wizardContent.querySelector(`.wizard-eyes`).style.fill = wizard.eyeColor;
 
-  return wizardElement;
+  return wizardContent;
 };
 
-let fragment = document.createDocumentFragment();
+const feelingFragment = function () {
+  const fragment = document.createDocumentFragment();
 
-for (let i = 0; i < randomWizardList.length; i++) {
-  fragment.appendChild(renderWizard(randomWizardList[i]));
-}
+  randomWizardsList.forEach(function (item) {
+    fragment.appendChild(renderWizard(item));
+  });
 
-similarListElement.appendChild(fragment);
+  similarList.appendChild(fragment);
+};
+
+feelingFragment();
 
 userDialog.querySelector(`.setup-similar`).classList.remove(`hidden`);
